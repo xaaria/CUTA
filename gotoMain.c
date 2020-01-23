f83d10892799699e85d6e87832b04901884b3b9e
Xaaria <okkomarble@gmail.com>
2020-01-23 13:19:57 +0200

    toimiva versio goto:sta, tosin ilman gotoja!

--- gotoMain.c
+++ gotoMain.c
@@ -13,15 +13,14 @@ int main(int argc, char *argv[])
 {
   int kork = atoi(argv[1]);
   int lev = atoi(argv[2]);
-  int **t2d = malloc(kork*sizeof(int *));
+  int** t2d = malloc(kork*sizeof(int *));
   int i = 0, j = 0;
   int max1 = INT_MIN, max2 = INT_MIN;
   srand(time(NULL));
   for(i = 0; i < kork; ++i)
   {
     t2d[i] = malloc(lev*sizeof(int));
-    for(j = 0; j < lev; ++j)
-    {
+    for(j = 0; j < lev; ++j) {
       t2d[i][j] = rand();
       max1 = (max1 < t2d[i][j]) ? t2d[i][j] : max1;
     }
--- gotokoe.c
+++ gotokoe.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 #include "gotokoe.h"
 /* gotokoe.c */
 
@@ -9,34 +10,40 @@
   Parametri kork kertoo taulukon korkeuden (ensimmäistä indeksiä vastaavan ulottuvuuden koko) 
   ja lev leveyden (toista indeksiä vastaavan ulottuvuuden koko).
 
-  t2d --> int[3][3]: 
-    [][][],
-    [][][],
-    [][][]
+  t2d --> int*[int*, ...] --> [int, int, int]: 
 
 */
 
 int max2D(int** t2d, int kork, int lev) {
 
-  int max = 0;
-  int* t = *t2d; /* hae taulun alku pointterista */
+  int max = INT_MIN;
+  int** t = t2d; /* hae taulun alku pointterista */
 
   unsigned int r = 0; /* rivit-iteraattori */
   unsigned int c = 0; /* sarake-iteraattori */
 
   /* Rivin maksimi */
-  for(r; r<kork; r++) {
-    for(c; c<lev; c++) {
-      int* rivi = (int*) t[r];
+  for(r=0; r<kork; ++r) {
+
+    /* vaihda riviä */
+    int* rivi = t[r];
+
+    /* käy rivin arvot läpi */
+    for(c=0; c<lev; ++c) {
+      
+      printf("olemme paikassa <%d,%d>\n", r, c);
+
       int val = rivi[c];
+      printf("> %d\n", val);
       if( val > max) {
-        printf("%d", val);
+        printf("\tuusi maksimi %d (%d)\n", val, max);
         max = val; /* päivitä */
       }
     }
+
   }
 
-  printf("return %d", max);
+  printf("return %d\n", max);
   return max;
 
 }
